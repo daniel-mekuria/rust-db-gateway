524bdf9f2890369160c08115167fee3b6622072c
daniel-mekuria <mekuriadaniel4@gmail.com>
2025-07-02 12:05:22 +1000

    fix: add_encrypted_constraint

--- tests/sql/schema.sql
+++ tests/sql/schema.sql
@@ -157,6 +157,8 @@ SELECT eql_v2.add_search_config(
   '{"prefix": "encrypted/encrypted_jsonb"}'
 );
 
+SELECT eql_v2.add_encrypted_constraint('encrypted', 'encrypted_text');
+
 
 -- This is the exact same schema as above but using a database-generated primary key.
 -- It is required to remove flake form the Elixir integration test suite.
@@ -298,4 +300,6 @@ SELECT eql_v2.add_search_config(
   'jsonb',
   '{"prefix": "encrypted/encrypted_jsonb"}'
 );
+
+SELECT eql_v2.add_encrypted_constraint('encrypted_elixir', 'encrypted_text');
 
